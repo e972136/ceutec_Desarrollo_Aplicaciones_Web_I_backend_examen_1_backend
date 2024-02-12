4e9be490b00c7b6d2b782920dcacb89fb0065dd3
Gashpar <e972136@unitec.edu>
2024-02-11 20:03:44 -0600

    Terminado reparacion

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { estimacion } from './routes/apiEstimacion.js';
 import { aseguradora } from './routes/apiAseguradora.js';
+import { reparacion } from './routes/apiReparacion.js';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/estimacion",estimacion);
 app.use("/api/aseguradora",aseguradora);
+app.use("/api/reparacion",reparacion);
 
 app.listen(port,()=>{
     console.log(`Se escucha en ${port}`);
