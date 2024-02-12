510034346b9c71ff3490389afe562ba01660480f
Gaspar <e972136@unitec.edu>
2024-02-12 14:44:58 -0600

    Agregado reparacion y repuesto

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { estimacion } from './routes/apiEstimacion.js';
 import { aseguradora } from './routes/apiAseguradora.js';
 import { reparacion } from './routes/apiReparacion.js';
+import { reparacionAdicional } from './routes/apiReparacionAdicional.js';
+import { repuesto } from './routes/apiRepuesto.js';
 
 const app = express();
 
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use("/api/estimacion",estimacion);
 app.use("/api/aseguradora",aseguradora);
 app.use("/api/reparacion",reparacion);
+app.use("/api/reparacion-adicional",reparacionAdicional);
+app.use("/api/repuesto",repuesto);
 
 app.listen(port,()=>{
     console.log(`Se escucha en ${port}`);
