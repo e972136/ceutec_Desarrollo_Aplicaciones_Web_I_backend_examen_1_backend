076fb219661f2c61222919c8cadb5d28740e6628
Gashpar <e972136@unitec.edu>
2024-03-01 21:44:50 -0600

    Integracion corss origin

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { aseguradora } from './routes/apiAseguradora.js';
 import { reparacion } from './routes/apiReparacion.js';
 import { reparacionAdicional } from './routes/apiReparacionAdicional.js';
 import { repuesto } from './routes/apiRepuesto.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -13,6 +14,15 @@ const port = 9090;
 //middlewares
 app.use(express.json());
 
+const corsOptions = {
+    origin : 'http://localhost:5173', 
+    credentials : true,
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+app.use(cors(corsOptions));
+
+//Rutas
 app.use("/api/estimacion",estimacion);
 app.use("/api/aseguradora",aseguradora);
 app.use("/api/reparacion",reparacion);
